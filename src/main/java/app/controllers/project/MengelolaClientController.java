154c6afbcddd727666879109c8b660159f42ccdb
khairil89 <khairil.anwar852@gmail.com>
2018-08-01 10:01:06 +0700

    ah

--- src/main/java/app/controllers/project/MengelolaClientController.java
+++ src/main/java/app/controllers/project/MengelolaClientController.java
@@ -30,7 +30,6 @@ public class MengelolaClientController extends CRUDController<Clients> {
     
     @Override
 	public Clients customInsertValidation(Clients item) throws Exception {
-    	List<Map<String, Object>> listMapClient = new ArrayList<Map<String, Object>>();
     	LazyList<Clients> listClient = Clients.findAll();
     	
     	String clientName = item.getString("CLIENT_NAME");
