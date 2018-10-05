29f58d59c2d8323728976611a02a9352291ed8f8
rahmadn24 <r4hmadn@gmail.com>
2018-10-05 09:31:31 +0700

    Edit Controller MengelolaHiring, dan model SdmAssignment,SdmHiring

--- src/main/java/app/controllers/project/MengelolaHiringController.java
+++ src/main/java/app/controllers/project/MengelolaHiringController.java
@@ -1,5 +1,8 @@
 package app.controllers.project;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 /**
  *web-seed
  * MengelolaHiring.java
@@ -9,13 +12,18 @@ package app.controllers.project;
  */
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections4.iterators.SkippingIterator;
 import org.javalite.activejdbc.LazyList;
 import org.javalite.activejdbc.Model;
+import org.javalite.activeweb.annotations.GET;
+import org.javalite.activeweb.annotations.PUT;
 import org.javalite.common.Convert;
+
+import com.ibm.icu.util.Calendar;
 
 import app.controllers.api.masterdata.SkillController.SkillDTO;
 import app.models.Clients;
@@ -25,6 +33,7 @@ import app.models.SdmHiring;
 import app.models.Skill;
 import app.models.SkillType;
 import app.models.StatusHiring;
+import core.io.enums.HttpResponses;
 import core.io.helper.Validation;
 import core.io.model.CorePage;
 import core.io.model.DTOModel;
@@ -63,6 +72,7 @@ public class MengelolaHiringController extends CRUDController<SdmHiring>{
 		// Modified : Hendra Kurniawan
 		// Date 	: 12-09-2018
 		List<Map> listdata = new ArrayList<>();
+		List<Map> listdata2 = new ArrayList<>();
 		
 		listdata = SdmHiring.getDataSdmbyEndProject();
 		
@@ -75,9 +85,26 @@ public class MengelolaHiringController extends CRUDController<SdmHiring>{
 			sdmhiringId = Convert.toInteger(dataSdm.get("sdmhiring_id"));
 //			SdmHiring.updateHireStatIdbyClient(sdmhiringId);
 //			SdmHiring.updateHireStatIdbyClient79(sdmId, 1);
-			System.out.println("SDM" + sdmId);
-			System.out.println("Client" + clientId);
 		}
+		
+//		for(Map dataSdm : listdata2) {
+//			sdmId1 = Convert.toInteger(dataSdm.get("sdm_id"));
+//			clientId1 = Convert.toInteger(dataSdm.get("client_id"));
+//			sdmhiringId1 = Convert.toInteger(dataSdm.get("sdmhiring_id"));
+//			hireId = Convert.toInteger(dataSdm.get("hirestat_id"));
+//			System.out.println("Ini listdata2");
+//			System.out.println(clientId1);
+//			System.out.println(hireId);
+//			
+//			if((clientId1 == 1 && hireId != 4) || (clientId1 != 1 && hireId != 4)) {
+//				System.out.println("Masuk sini gak");
+//				SdmHiring.updateHireStat79(sdmhiringId1);
+//				
+//			}
+////			else {
+////				System.out.println("Si "+sdmhiringId1+" ngubah");
+////			}
+//		}
 		
 		/*
 		* Created By  : Rizaldi
@@ -111,71 +138,73 @@ public class MengelolaHiringController extends CRUDController<SdmHiring>{
 		return new CorePage(listMapHiring, totalItems);		
 	}
 	
+	public String getCurrentDate(){
+	    final Calendar c = Calendar.getInstance();
+	    int year, month, day;
+	    year = c.get(Calendar.YEAR);
+	    month = c.get(Calendar.MONTH);
+	    day = c.get(Calendar.DATE);
+	    return year + "-" + (month+1) + "-" + day ;
+	}
 	
-//	public Map<String, Object> customOnInsert(SdmHiring item, Map<String, Object> mapRequest) throws Exception {
-//		Map<String, Object> result = super.customOnInsert(item, mapRequest);
-//		HiringDTO dto = new HiringDTO();
-//		dto.fromModelMap(result);
-//		
-//		Sdm sdm = item.parent(Sdm.class);
-//		StatusHiring hirestat = item.parent(StatusHiring.class);
-//		Clients client = item.parent(Clients.class);
-//		dto.sdmName = Convert.toString(sdm.get("sdm_name"));
-//		dto.hirestatName = Convert.toString(hirestat.get("hirestat_name"));
-//		dto.clientName = Convert.toString(client.get("client_name"));
-//
-//		return dto.toModelMap();
-//	}
-//	
-//	public SdmHiring customInsertValidation(SdmHiring item) throws Exception {
-//		Integer sdmId = item.getInteger("sdm_id");
-//		Integer hirestatId = item.getInteger("hirestat_id");
-//		Integer clientId = item.getInteger("client_id");
-//		Integer sdmhiringId = item.getInteger("sdmhiring_id");
-//		
-//		// Contoh Validasi untuk variable yang harus memiliki nilai / tidak boleh null
-//		Validation.required(sdmId, "Id Sdm Tidak Boleh Kosong");
-//		Validation.required(hirestatId, "Id Status Hire tidak boleh kosong");
-//		Validation.required(clientId, "Id Client tidak boleh kosong");
-//		Validation.required(sdmhiringId, "Id hiring tidak boleh kosong");
-//
-//		return super.customInsertValidation(item);
-//	}
+	@SuppressWarnings("rawtypes")
+	@PUT
+	public void updateSdm() throws ParseException { 
+	int sdmhiring_id = Convert.toInteger(param("sdmhiring_id"));
+	int hireStat_id = Convert.toInteger(param("hirestat_id"));
+	int client_id = Convert.toInteger(param("client_id"));
 	
-//	public Map<String, Object> customOnDelete(SdmHiring item, Map<String, Object> mapRequest) throws Exception {
-//			
-//		Map<String, Object> result = super.customOnDelete(item, mapRequest);		
-//		HiringDTO dto = new HiringDTO();
-//		dto.fromModelMap(result);
-//		
-//		Sdm sdm = item.parent(Sdm.class);
-//		StatusHiring hirestat = item.parent(StatusHiring.class);
-//		Clients client = item.parent(Clients.class);
-//		dto.sdmName = Convert.toString(sdm.get("sdm_name"));
-//		dto.hirestatName = Convert.toString(hirestat.get("hirestat_name"));
-//		dto.clientName = Convert.toString(client.get("client_name"));
-//		
-//		return dto.toModelMap();
-//	}
 	
-//	@Override
-//	public Map<String, Object> customOnUpdate(SdmHiring item, Map<String, Object> mapRequest) throws Exception {
-//				
-//		Map<String, Object> result = super.customOnUpdate(item, mapRequest);
-//		HiringDTO dto = new HiringDTO();
-//		dto.fromModelMap(result);
-//		
-//		Sdm sdm = item.parent(Sdm.class);
-//		StatusHiring hirestat = item.parent(StatusHiring.class);
-//		Clients client = item.parent(Clients.class);
-//		dto.sdmhiringId = Convert.toInteger(hirestat.get("sdmhiring_id"));
-//		dto.sdmName = Convert.toString(sdm.get("sdm_name"));
-//		dto.hirestatName = Convert.toString(hirestat.get("hirestat_name"));
-//		dto.clientName = Convert.toString(client.get("client_name"));
-//		
-//		return dto.toModelMap();
-//	}
-
+//	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//	Date curren = new Date();
+//	System.out.println(client_id);
+	String currentDate = getCurrentDate();
+//	System.out.println(currentDate);
+//	Date current = sdf.parse(currentDate);
+//	System.out.println(client_id);
+//	String endDate = Convert.toString(current);
+	
+	System.out.println(sdmhiring_id);
+		List<Map> listdata2 = new ArrayList<>();
+		int sdmId1, clientId1, sdmhiringId1, hireId;
+		try {
+		SdmHiring.updateHire(sdmhiring_id, hireStat_id);
+		if(hireStat_id == 9) {
+			System.out.println("Update Masuk");
+			System.out.println(currentDate);
+			System.out.println(sdmhiring_id);
+			System.out.println(client_id);
+			SdmAssignment.updateEndDateCv79(currentDate, sdmhiring_id, client_id);
+		}
+		listdata2 = SdmHiring.getDataSdmbyOnProject(sdmhiring_id);
+		for(Map dataSdm : listdata2) {
+			clientId1 = Convert.toInteger(dataSdm.get("client_id"));
+			sdmhiringId1 = Convert.toInteger(dataSdm.get("sdmhiring_id"));
+			hireId = Convert.toInteger(dataSdm.get("hirestat_id"));
+			sdmId1 = Convert.toInteger(dataSdm.get("sdm_id"));
+			String akhirKontrak = Convert.toString(dataSdm.get("sdm_endcontract"));
+			System.out.println("Ini listdata2");
+			System.out.println(clientId1);
+			System.out.println(hireId);
+			if(hireId != 4) {
+				if(clientId1 == 1 && hireId != 4) {
+					System.out.println("Masuk sini gak");
+					SdmHiring.updateHireStat79(sdmhiringId1);
+					SdmAssignment.updateStartDateEnddateAssignCv79(clientId1,  sdmhiringId1, currentDate, akhirKontrak);
+				}
+			}
+			else {
+				
+			}
+			response().setResponseBody(HttpResponses.ON_SUCCESS);
+		}
+		}catch(Exception e) {
+			response().setResponseBody(e, 400);
+		}
+		sendResponse();
+		
+		//return result;
+	}
 	//Modified by : Dewi Roaza
     //Date        : 10/09/2018 
     @Override
